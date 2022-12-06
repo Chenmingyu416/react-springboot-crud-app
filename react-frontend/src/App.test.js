e1fbd936d48bd265f1bc4aa549598a828c18a3d5
Mingyu Chen <chen_mingyu416@sina.com>
2022-12-06 11:34:20 -0500

    add engine

--- react-frontend/src/App.test.js
+++ react-frontend/src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // const element = render(<App />);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+  const { debug } = render(<App />);
+  debug();
 });
+
